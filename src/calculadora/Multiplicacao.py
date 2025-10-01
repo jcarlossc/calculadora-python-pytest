7298b04f8badf745c507fa5f038fb5a5e6667edc
jcarlossc <josecscosta@fac.pe.senac.br>
2025-10-01 18:44:50 -0300

    Classe Multiplicação.

--- src/calculadora/Multiplicacao.py
+++ src/calculadora/Multiplicacao.py
@@ -1,10 +1,10 @@
 from calculadora.Operacao import Operacao
 
-class Subtracao(Operacao):
+class Multiplicacao(Operacao):
 
     def executar(self, num1: float, num2: float) -> float:
 
         try:
-            return num1 - num2
+            return num1 * num2
         except Exception as e:
             print(f"Erro: {e}")
